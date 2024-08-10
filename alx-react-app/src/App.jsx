949ed5cc654fce30005aec665d1454d329ed7fc5
Abdul Aziz <abdulalhassan1199@gmail.com>
2024-08-10 23:23:08 +0000

    header,main and footer added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,13 @@
-<<<<<<< HEAD
+
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 import WelcomeMessage from "./components/WelcomeMessage";
 
-=======
->>>>>>> b7f592757f00876b89635dfc842f6b8ce95e017c
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -33,11 +33,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-<<<<<<< HEAD
 
       <WelcomeMessage />
-=======
->>>>>>> b7f592757f00876b89635dfc842f6b8ce95e017c
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
