974c3fed37dfde4758da9a3b3fd4d64bced7ff8b
Abdul Aziz <abdulalhassan1199@gmail.com>
2024-08-11 09:53:33 +0000

    changes made to react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 import { useState } from 'react'
@@ -38,6 +39,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name='Abdul Aziz' age='26' bio='Loves coding' />
     </>
   )
 }
