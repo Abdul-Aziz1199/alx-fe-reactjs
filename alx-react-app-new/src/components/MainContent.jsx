331cfa1f82d79bc0b7d2e516e51d526975a48416
Abdul Aziz <abdulalhassan1199@gmail.com>
2024-08-16 22:34:04 +0000

    maincontent and footer added

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return(
-        <main>
+        <main style={{backgroundColor:'lightgray'}}>
               <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     )
