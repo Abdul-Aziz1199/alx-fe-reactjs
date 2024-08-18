26770905ab0369b5d76c5c54002d2c3a674ac2c9
Abdul Aziz <abdulalhassan1199@gmail.com>
2024-08-18 22:21:06 +0000

    company project initial commit

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ padding: '20px', backgroundColor: '#ccc', textAlign: 'center' }}>
+      <p>&copy; 2024 My Company. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
