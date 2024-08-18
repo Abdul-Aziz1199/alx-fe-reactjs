475d6eaa7e477be23b33f2d2235cbf4fc11f5f54
Abdul Aziz <abdulalhassan1199@gmail.com>
2024-08-18 00:01:44 +0000

    updated

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example,com" };
 
   return (
-    <UserProvider value={userData}>
+    <UserContext.Provider value={userData}>
         <ProfilePage />
-    </UserProvider>
+    </UserContext.Provider>
 
   );
 }
