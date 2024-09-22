3ee5876fb62cc52738e92da1a9d3ab17d9b8598b
Abdul Aziz <abdulalhassan1199@gmail.com>
2024-09-22 08:30:03 +0000

    removed

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SearchBar from './components/searchBar';
 
 const Home = () => <h1>Welcome to GitHub User Search</h1>;
 
