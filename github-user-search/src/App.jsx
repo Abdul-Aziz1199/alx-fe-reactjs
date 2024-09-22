8041d83b3aac4c6a72ed2c84df74df2321584a0e
Abdul Aziz <abdulalhassan1199@gmail.com>
2024-09-22 08:11:57 +0000

    components added

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+
 const Home = () => <h1>Welcome to GitHub User Search</h1>;
 
 function App() {
